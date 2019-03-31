ed770fb47b3b08cab04f09b6f2c9bf8afddceb9b
Robert Cecil <rhinobobby@gmail.com>
2019-03-30 21:18:00 -0400

    added buttons, removed boilerplate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'displayProject';
+  clickMessage: string;
+
+
+  onClickMe() {
+    this.clickMessage = 'You are my hero!';
+  }
+
+
 }
