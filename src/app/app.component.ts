bf7d92b4d054b80e15fb77ff04832b246c388219
Robert Cecil <rhinobobby@gmail.com>
2019-04-05 13:30:22 -0400

    added server connection abilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { Cat } from './models/cat';
+import { SerialportService } from './services/serialport.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -10,15 +13,22 @@ import {MatButtonModule} from '@angular/material/button';
 export class AppComponent {
   title = 'displayProject';
   clickMessage: string;
+  catString: string;
+  mycats: Observable<Cat[]>;
 
-  constructor() {
+  constructor(private catservice: SerialportService) {
 
   }
 
 
   onClickMe() {
     this.clickMessage = 'You are my hero!';
+    this.getCats();
   }
 
+  getCats() {
+    this.mycats = this.catservice.getAllCats();
+    this.mycats.subscribe((res) => console.log(res));
+  }
 
 }
